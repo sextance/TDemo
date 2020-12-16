42a3fdf06687730673221f41112613a1cb733091
Liu Yj <RaveChase@users.noreply.github.com>
2020-12-16 10:54:11 +0800

    1111
    v1

---
+++ Assets/Scripts/TowerScripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /*public ServerMsg... serverMsg.....*/
+    public TowerShapeFactory towerShapeFactory;
+
+
+    //KeyCode to call event
+    public KeyCode createAttackTower = KeyCode.Z;
+    public KeyCode createDefenseTower = KeyCode.X;
+    public KeyCode createProductionTower = KeyCode.C;
+    public KeyCode destroyTower = KeyCode.D;
+    public KeyCode newGameKey = KeyCode.N;
+
+    List<TowerShape> towerShapes;
+    void Start()
+    {
+        /* Reserve for Sence logic*/
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(createAttackTower) ) //需要交互 isZoneSelected
+            CreateTowerShape(0);
+        if (Input.GetKeyDown(createDefenseTower)) //需要交互 isZoneSelected
+            CreateTowerShape(1);
+        if (Input.GetKeyDown(createProductionTower)) //需要交互 isZoneSelected
+            CreateTowerShape(2);
+        if (Input.GetKeyDown(destroyTower)) //需要交互 isTowerSelected
+            DestroyTowerShape();
+    }
+
+    void CreateTowerShape(int towerId)
+    {
+        TowerShape instance = towerShapeFactory.Get(towerId, towerId);
+        Transform t = instance.transform;
+        /*Reserve*/
+        t.localPosition = Random.insideUnitSphere * 5f;
+
+        towerShapes.Add(instance);
+    }
+
+    void DestroyTowerShape()
+    {
+        if(towerShapes.Count > 0)
+        {
+            int index = Random.Range(0, towerShapes.Count);
+            towerShapeFactory.Reclaim(towerShapes[index]);
+
+            //Switch the index of selected and last one
+            int lastIndex = towerShapes.Count - 1;
+            towerShapes[index] = towerShapes[lastIndex];
+            towerShapes.RemoveAt(lastIndex);
+        } else {
+            Debug.LogError("No tower in pools to destroy!");
+        }
+    }
+
+}
---
+++ Assets/Scripts/TowerScripts/TowerShapeFactory.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class TowerShapeFactory : ScriptableObject
+{
+    [SerializeField]
+    TowerShape[] prefabs;
+
+    //The order of elements is Attack, Defense, Production
+    [SerializeField]
+    Material[] materials;
+
+    [SerializeField]
+    bool recycle;
+    List<TowerShape>[] pools;
+
+    Scene poolScene;
+
+    //Get new tower from factory
+    public TowerShape Get(int shapeId = 0, int materialId = 0)
+    {
+        TowerShape instance;
+
+        if (shapeId != materialId)
+            Debug.LogError("Different material and tower id input!");
+
+        if (recycle)
+        {
+            if (pools == null)
+                CreatePools();
+
+            //Recycle last object of pool to enssure consistency of list
+            List<TowerShape> pool = pools[shapeId];
+            int lastIndex = pool.Count - 1;
+            if(lastIndex >= 0)
+            {
+                instance = pool[lastIndex];
+                instance.gameObject.SetActive(true);
+                pool.RemoveAt(lastIndex);
+                /* ... */
+            } else {
+                instance = Instantiate(prefabs[shapeId]);
+                instance.ShapeId = shapeId;
+                SceneManager.MoveGameObjectToScene(instance.gameObject, poolScene);
+            }
+        } else {
+            instance = Instantiate(prefabs[shapeId]);
+            instance.ShapeId = shapeId;
+        }
+
+        instance.SetMaterial(materials[materialId], materialId);
+        return instance;
+    }
+
+    //Put destroyed tower back to factory
+    public void Reclaim(TowerShape shapeToRecycle)
+    {
+        if (recycle)
+        {
+            if (pools == null)
+                CreatePools();
+            pools[shapeToRecycle.ShapeId].Add(shapeToRecycle);
+            shapeToRecycle.gameObject.SetActive(false);
+        } else {
+            Destroy(shapeToRecycle.gameObject);
+        }
+    }
+
+    void CreatePools()
+    {
+        pools = new List<TowerShape>[prefabs.Length];
+        for (int i = 0; i < pools.Length; i++)
+            pools[i] = new List<TowerShape>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
