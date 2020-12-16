6aff73adb3e880aebba6e279e8f492cd45454529
Endor0069 <76037122+Endor0069@users.noreply.github.com>
2020-12-16 13:33:35 +0800

    Enemy Add

---
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    EnemyFactory originFactory;
+
+    [SerializeField]
+    NavMeshAgent navEnemy = default;
+
+    [SerializeField]
+    Transform TargetPoint = default;
+
+    public float Health { get; set; }
+
+    float attack;
+    float attackRange;
+    float tauntRange;
+
+    private void Update()
+    {
+        FindAndGo();
+        Attack();
+    }
+
+    public EnemyType enemyType => EnemyType.common;
+
+    public EnemyFactory OriginFactory
+    {
+        get => originFactory;
+        set
+        {
+            Debug.Assert(originFactory == null, "Redefined origin Factory!");
+            originFactory = value;
+        }
+    }
+
+    public void Initialize(float health,float attack,float attackingRange,float tauntRange)
+    {
+        this.attack = attack;
+        this.attackRange = attackingRange;
+        this.tauntRange = tauntRange;
+        Health = health;
+    }
+
+    public bool GameUpdate()
+    {
+        if(this == null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    bool FindAndGo()
+    {
+        navEnemy.SetDestination(TargetPoint.position);
+        return true;
+    }
+
+    void Attack()
+    {
+        if(navEnemy.velocity.x == 0 && navEnemy.velocity.z == 0)
+        {
+            Debug.Log("Attack Tower");
+        }
+    }
+
+    void ApplyDamge(float damge)
+    {
+        Health -= damge;
+    }
+
+    public enum EnemyType
+    {
+        T,DPS,common
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/Game.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    EnemyFactory enemyFactory;
+
+    [SerializeField]
+    float spawnSpeed = 1f;
+
+    [SerializeField]
+    float time = 0f;
+
+    [SerializeField]
+    float enemySpawnSpeed1 = 0f;
+
+    [SerializeField]
+    float enemySpawnSpeed2 = 0f;
+
+    [SerializeField]
+    float timeLimit = 0f;
+
+    EnemyCollections enemies = new EnemyCollections();
+
+    List<Vector3> Position = new List<Vector3>();
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        TimeToSpawn();
+        enemies.GameUpdate();
+
+    }
+
+    void SpawnEnemy(float enemySpawnSpeed)//生成敌人
+    {
+        spawnSpeed += spawnSpeed * Time.deltaTime;
+        while (spawnSpeed >= enemySpawnSpeed)
+        {
+            spawnSpeed -= enemySpawnSpeed;
+            Enemy enemy = enemyFactory.Get();
+            enemies.Add(enemy);
+        }
+    }
+
+    void TimeToSpawn()//随时间限制改变怪物生成速度
+    {
+        time += Time.deltaTime;
+        SpawnEnemy(enemySpawnSpeed1);
+        if(time > timeLimit)
+        {
+            SpawnEnemy(enemySpawnSpeed2);
+        }
+    }
+
+    void SpawnEnemyAround()//获取边缘坐标，生成敌人
+    {
+        for (int i = 0; i < 8; i += 7)
+        {
+            for (int j = 0; j < 12; j += 11)
+            {
+                Position[i] = this.transform.position;
+                Enemy enemy = Instantiate(enemyFactory.enemyPrefab, Position[i], Quaternion.identity);
+            }
+        }
+    }
+
+    void TimeToSpawnAround()
+    {
+        time += Time.deltaTime;
+        if(time > 120f)
+        {
+            for(int i= 0; i < 5; i++)
+            {
+                spawnSpeed += spawnSpeed * Time.deltaTime;
+                if(spawnSpeed >= 1f)
+                {
+                    SpawnEnemyAround();
+                    spawnSpeed -= 1f;
+                }
+            }
+        }
+        else if(time > 300f)
+        {
+            SpawnEnemyAround();
+        }
+    }
+}
