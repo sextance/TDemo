09fce34f276d140c9397c5c239bb489ca1d0aaf8
sextance <60652432+sextance@users.noreply.github.com>
2020-12-16 23:16:00 +0800

    Add Scene Switch

---
+++ Assets/Scripts/UIScipts/SceneSwitch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnStartGame(int SceneNumber)
+    {
+        Debug.Log("pve");
+        SceneManager.LoadScene(SceneNumber);
+    }
+}
